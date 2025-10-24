df3625c501ab1796345b22bd0fd8aa94d04990ae
VuVietNghia <vvn0068@gmail.com>
2025-10-24 10:35:06 +0700

    May tinh don gian

---
+++ HelloWorld.py
@@ -0,0 +1,21 @@
+def mayTinh():
+    so1_input = input("Nhap so a: ")
+    so1 = float(so1_input)
+
+    so2_input = input("Nhap so b: ")
+    so2 = float(so1_input)
+
+    phepTinh = input("Nhap phep tinh(+ - * /): ")
+    so3 = None
+
+    if phepTinh == '+':
+        so3 = so1 + so2
+    elif phepTinh == '-':
+        so3 = so1 + so2
+    elif phepTinh == '*':
+        so3 = so1 + so2
+    elif phepTinh == '/':
+        if so2 == 0:
+            print("Khong the chia het cho 0")
+        so3 = so1 / so2
+mayTinh()
